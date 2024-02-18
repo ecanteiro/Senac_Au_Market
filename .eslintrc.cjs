5e850c0095044a50d166373b052656fabb7c7ad1
Marcus Pereira <pereiramarcus93@gmail.com>
2024-02-18 13:36:25 -0300

    chore: update eslint settings to make sure the indentation will follow two space rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'indent': ['warn', 2],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
