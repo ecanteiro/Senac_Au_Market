43d002936703f03b2c9671328cb2c96700b79b5e
Marcus Pereira <pereiramarcus93@gmail.com>
2024-02-18 13:38:44 -0300

    chore: eslint indent

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerButton = document.getElementById("hamburger");
+  const mobileNav = document.querySelector(".mobile-nav");
 
-	document.addEventListener("DOMContentLoaded", function () {
-		const hamburgerButton = document.getElementById("hamburger");
-		const mobileNav = document.querySelector(".mobile-nav");
-	  
-		hamburgerButton.addEventListener("click", function () {
-		  mobileNav.classList.toggle("is-active");
-		});
-	  });
+  hamburgerButton.addEventListener("click", function () {
+    mobileNav.classList.toggle("is-active");
+  });
+});
