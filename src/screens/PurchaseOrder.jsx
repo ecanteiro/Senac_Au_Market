7cd13b2ef76e428d59a2d45382dea190202b156a
Marcus Pereira <pereiramarcus93@gmail.com>
2024-03-10 22:04:45 -0300

    chore: pequenos ajustes na tela de produto e carrinho de compras.

--- src/screens/PurchaseOrder.jsx
+++ src/screens/PurchaseOrder.jsx
@@ -1,71 +1,44 @@
 import { Link } from 'react-router-dom';
+import {CommonContainer} from "../components/CommonContainer.jsx";
+import RegistrationHeader from "../components/RegistrationHeader.jsx";
 function PurchaseOrder() {
   return (
-    <>
-      <div className="container is-fluid">
-        <section className="mt-5">
-          <div className="columns">
-            <div className="column">
-              <h1 className="subtitle"
-                style={
-                  {color:'#06B3C4',
-                    textAlign:"center",
-                    fontSize:"20px",
-                    marginBottom:"3px",
-                    fontWeight:"600"}}>
+    <CommonContainer>
+      <RegistrationHeader
+        title="Pedidos"
+        subtitle="Acompanhe seus pedidos"
+      />
 
-                                    Pedidos</h1>
-              <h2 style={
-                {color:'#777777',
-                  textAlign:"center",
-                  fontSize:"14px",
-                  fontWeight:"600",
-                  margin:"16px 80px"}}>Meus pedidos</h2>
+      <div className="rectangle">
+        <h1 style={
+          {
+            color: '#06B3C4',
+            textAlign: "left",
+            fontWeight: "600",
+          }}>Ração Úmida Dog NaturalFood</h1>
+        <p>Tamanho: <b>200g</b></p>
+        <p>Sabor: <b>Carne</b></p>
+        <p style={
+          {
+            color: '#777777',
+            textAlign: "right",
+            fontSize: "10px",
+            fontWeight: "600",
+          }}>
+          Número do pedido: 884879
+        </p>
 
-            </div>
-          </div>
-          <div>
-            <h2 style={
-              {color:'#06B3C4',
-                textAlign:"center",
-                fontSize:"16px",
-                fontWeight:"600",
-                margin:"16px 80px"}}>Acompanhe seu pedido</h2>
-          </div>
-
-          <div className="rectangle">
-
-            <h2 style={
-              {color:'#06B3C4',
-                textAlign:"left",
-                fontSize:"14px",
-                fontWeight:"600",
-                margin:"16px 18px"}}>Ração úmida dog NaturalFood - Pacote 200g(Carne, Frango ou Suíno).</h2>
-            <h1 style={
-              {color:'#777777',
-                textAlign:"right",
-                fontSize:"10px",
-                fontWeight:"600",
-                margin:"-10px 20px"}}>Número do pedido: 884879 </h1>
-            <ol>
-              <li>
-                                        Ração úmida dog NaturalFood
-              </li>
-            </ol>
-            <Link to={"/shopping-cart"}>
-              <button className="button is-rounded is-primary-au-market">
-                <span className="icon">
-                  <i className="fa-solid fa-arrow-right"></i>
-                </span>
-                <span>Avançar</span>
-              </button>
-            </Link>
-          </div>
-
-        </section>
+        <Link to={"/shopping-cart"}>
+          <button className="button is-rounded is-primary-au-market">
+            <span className="icon">
+              <i className="fa-solid fa-arrow-right"></i>
+            </span>
+            <span>Avançar</span>
+          </button>
+        </Link>
       </div>
-    </>
-  )
-}
+
+    </CommonContainer>
+  )}
 
 export default PurchaseOrder
